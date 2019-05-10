b111159c034c15c2ed85ab89e26908ff1e0ab4a1
Tim Davis <davis@tamu.edu>
2019-05-10 13:52:13 -0500

    testall3

--- Test/testall3.m
+++ Test/testall3.m
@@ -1,8 +1,11 @@
-nthreads_set (2)
-logstat ('test16') ;  % user-defined complex operators
-logstat ('test18') ;  % quick tests of GrB_eWiseAdd and eWiseMult
-logstat ('test75') ;  % test GrB_mxm A'*B on all semirings
-logstat ('test06') ;  % test GrB_mxm on all semirings
-logstat ('test19b') ; % GrB_assign, many pending operators
-logstat ('test20') ;  % quick test of GB_mex_mxm on a few semirings
 
+clear all
+make
+gb
+for k = [1 2 4]
+    nthreads_set (k) ;
+    debug_off ;
+    testall 
+    debug_on
+    testall 
+end
