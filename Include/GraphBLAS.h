ce89e55b1ff7d50b19e4ba98e82ecb45ab668727
Tim Davis <davis@tamu.edu>
2019-06-30 07:27:15 -0500

    June 30 (7am) version

--- Include/GraphBLAS.h
+++ Include/GraphBLAS.h
@@ -71,7 +71,7 @@
 
 // The version of this implementation, and the GraphBLAS API version:
 #define GxB_IMPLEMENTATION_NAME "SuiteSparse:GraphBLAS"
-#define GxB_IMPLEMENTATION_DATE "June 29, 2019 (DRAFT)"
+#define GxB_IMPLEMENTATION_DATE "June 30, 2019 (7am) (DRAFT)"
 #define GxB_IMPLEMENTATION_MAJOR 3
 #define GxB_IMPLEMENTATION_MINOR 0
 #define GxB_IMPLEMENTATION_SUB   0
