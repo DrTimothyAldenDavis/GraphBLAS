0de8a5f03a8e704a4a524c710baf157f1391f021
Tim Davis <davis@tamu.edu>
2019-07-23 17:05:35 -0500

    draft sorting methods

---
+++ sort/sort.h
@@ -0,0 +1,22 @@
+//**********************************************************
+#define Int long
+void ssmergesort
+(
+    Int  A [ ],      /* array to sort, of size n */
+    Int  W [ ],      /* workspace of size n */
+    Int  n
+);
+
+void parsort1
+(
+    Int  A [ ],      /* array to sort, of size n */
+    Int  W [ ],      /* workspace of size n */
+    Int  n
+);
+
+void parsort2
+(
+    Int  A [ ],      /* array to sort, of size n */
+    Int  W [ ],      /* workspace of size n */
+    Int  n
+);
