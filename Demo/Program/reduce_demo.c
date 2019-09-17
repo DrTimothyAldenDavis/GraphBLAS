7ebdf73703a456a39758456fbf9a434dcb6d20d0
Tim Davis <davis@tamu.edu>
2019-09-17 11:36:12 -0500

    reduce demo

---
+++ Demo/Program/reduce_demo.c
@@ -0,0 +1,71 @@
+//------------------------------------------------------------------------------
+// GraphBLAS/Demo/Program/reduce_demo: reduce a matrix to a scalar
+//------------------------------------------------------------------------------
+
+#include "GraphBLAS.h"
+
+#define N 16384
+
+int main (void)
+{
+    // start GraphBLAS
+    GrB_init (GrB_NONBLOCKING) ;
+    printf ("demo: reduce a matrix to a scalar\n") ;
+
+    GrB_Index nrows = N ;
+    GrB_Index ncols = N ;
+    GrB_Matrix A ;
+    GrB_Matrix_new (&A, GrB_INT32, nrows, ncols) ;
+
+    for (int i = 0 ; i < nrows ; i++)
+    {
+        for (int j = 0 ; j < ncols ; j++)
+        {
+            int x = (int) (rand ( ) & 0xFF) ;
+            GrB_Matrix_setElement (A, x, i, j) ;
+        }
+    }
+
+    GrB_Index anz ;
+    GrB_Matrix_nvals (&anz, A) ;
+    GxB_print (A, 2) ;
+
+    int nthreads_max ;
+    GxB_get (GxB_NTHREADS, &nthreads_max) ;
+    fprintf (stderr, "# of threads: %d\n", nthreads_max) ;
+
+    GrB_Index result ;
+
+    #if defined ( _OPENMP )
+    double t = omp_get_wtime ( ) ;
+    #endif
+    GrB_reduce (&result, NULL, GxB_PLUS_INT32_MONOID, A, NULL) ;
+    #if defined ( _OPENMP )
+    t = omp_get_wtime ( ) - t ;
+    printf ("time: %g (default # of threads)\n", t) ;
+    #endif
+
+    double t1 ;
+
+    for (int nthreads = 1 ; nthreads <= nthreads_max ; nthreads++)
+    {
+        GxB_set (GxB_NTHREADS, nthreads) ;
+        #if defined ( _OPENMP )
+        double t = omp_get_wtime ( ) ;
+        #endif
+        GrB_reduce (&result, NULL, GxB_PLUS_INT32_MONOID, A, NULL) ;
+        #if defined ( _OPENMP )
+        t = omp_get_wtime ( ) - t ;
+        if (nthreads == 1) t1 = t ;
+        printf ("nthreads %3d time: %12.6fg speedup %8.2f\n", 
+            nthreads, t, t1/t) ;
+        #endif
+    }
+
+    printf ("result %d\n", result) ;
+
+    // free everyting
+    GrB_free (&A) ;
+    GrB_finalize ( ) ;
+}
+
