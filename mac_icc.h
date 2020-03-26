8ce7ce0a7ce5434b7bf7b0f40cb1f21d1321dedf
Tim Davis <DrTimothyAldenDavis@gmail.com>
2020-03-26 15:03:58 -0500

    remove malloc-options from icc

---
+++ mac_icc.h
@@ -0,0 +1,266 @@
+#define _LIBCPP_HAS_NO_RVALUE_REFERENCES 1
+#define _LIBCPP_HAS_NO_VARIADICS 1
+#define _LP64 1
+#define __APPLE_CC__ 5226
+#define __APPLE__ 1
+#define __ATOMIC_ACQUIRE 2
+#define __ATOMIC_ACQ_REL 4
+#define __ATOMIC_CONSUME 1
+#define __ATOMIC_HLE_ACQUIRE 65536
+#define __ATOMIC_HLE_RELEASE 131072
+#define __ATOMIC_RELAXED 0
+#define __ATOMIC_RELEASE 3
+#define __ATOMIC_SEQ_CST 5
+#define __BIGGEST_ALIGNMENT__ 64
+#define __BLOCKS__ 1
+#define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
+#define __CHAR16_TYPE__ unsigned short
+#define __CHAR32_TYPE__ unsigned int
+#define __CHAR_BIT__ 8
+#define __CLANG_ATOMIC_BOOL_LOCK_FREE 2
+#define __CLANG_ATOMIC_CHAR16_T_LOCK_FREE 2
+#define __CLANG_ATOMIC_CHAR32_T_LOCK_FREE 2
+#define __CLANG_ATOMIC_CHAR_LOCK_FREE 2
+#define __CLANG_ATOMIC_INT_LOCK_FREE 2
+#define __CLANG_ATOMIC_LLONG_LOCK_FREE 2
+#define __CLANG_ATOMIC_LONG_LOCK_FREE 2
+#define __CLANG_ATOMIC_POINTER_LOCK_FREE 2
+#define __CLANG_ATOMIC_SHORT_LOCK_FREE 2
+#define __CLANG_ATOMIC_WCHAR_T_LOCK_FREE 2
+#define __DATE__ "Mar 25 2020"
+#define __DBL_DECIMAL_DIG__ 17
+#define __DBL_DENORM_MIN__ 4.9406564584124654e-324
+#define __DBL_DIG__ 15
+#define __DBL_EPSILON__ 2.2204460492503131e-16
+#define __DBL_HAS_DENORM__ 1
+#define __DBL_HAS_INFINITY__ 1
+#define __DBL_HAS_QUIET_NAN__ 1
+#define __DBL_MANT_DIG__ 53
+#define __DBL_MAX_10_EXP__ 308
+#define __DBL_MAX_EXP__ 1024
+#define __DBL_MAX__ 1.7976931348623157e+308
+#define __DBL_MIN_10_EXP__ -307
+#define __DBL_MIN_EXP__ -1021
+#define __DBL_MIN__ 2.2250738585072014e-308
+#define __DEC128_EPSILON__ 1E-33DL
+#define __DEC128_MANT_DIG__ 34
+#define __DEC128_MAX_EXP__ 6145
+#define __DEC128_MAX__ 9.999999999999999999999999999999999E6144DL
+#define __DEC128_MIN_EXP__ -6142
+#define __DEC128_MIN__ 1E-6143DL
+#define __DEC128_SUBNORMAL_MIN__ 0.000000000000000000000000000000001E-6143DL
+#define __DEC32_EPSILON__ 1E-6DF
+#define __DEC32_MANT_DIG__ 7
+#define __DEC32_MAX_EXP__ 97
+#define __DEC32_MAX__ 9.999999E96DF
+#define __DEC32_MIN_EXP__ -94
+#define __DEC32_MIN__ 1E-95DF
+#define __DEC32_SUBNORMAL_MIN__ 0.000001E-95DF
+#define __DEC64_EPSILON__ 1E-15DD
+#define __DEC64_MANT_DIG__ 16
+#define __DEC64_MAX_EXP__ 385
+#define __DEC64_MAX__ 9.999999999999999E384DD
+#define __DEC64_MIN_EXP__ -382
+#define __DEC64_MIN__ 1E-383DD
+#define __DEC64_SUBNORMAL_MIN__ 0.000000000000001E-383DD
+#define __DECIMAL_BID_FORMAT__ 1
+#define __DECIMAL_DIG__ 21
+#define __DEC_EVAL_METHOD__ 2
+#define __DYNAMIC__ 1
+#define __EDG_PTRDIFF_TYPE__ long
+#define __EDG_SIZE_TYPE__ unsigned long
+#define __EDG_VERSION__ 500
+#define __EDG__ 1
+#define __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ 101403
+#define __FINITE_MATH_ONLY__ 0
+#define __FLOAT_WORD_ORDER__ __ORDER_LITTLE_ENDIAN__
+#define __FLT_DECIMAL_DIG__ 9
+#define __FLT_DENORM_MIN__ 1.40129846e-45F
+#define __FLT_DIG__ 6
+#define __FLT_EPSILON__ 1.19209290e-7F
+#define __FLT_HAS_DENORM__ 1
+#define __FLT_HAS_INFINITY__ 1
+#define __FLT_HAS_QUIET_NAN__ 1
+#define __FLT_MANT_DIG__ 24
+#define __FLT_MAX_10_EXP__ 38
+#define __FLT_MAX_EXP__ 128
+#define __FLT_MAX__ 3.40282347e+38F
+#define __FLT_MIN_10_EXP__ -37
+#define __FLT_MIN_EXP__ -125
+#define __FLT_MIN__ 1.17549435e-38F
+#define __FLT_RADIX__ 2
+#define __GCC_ATOMIC_BOOL_LOCK_FREE 2
+#define __GCC_ATOMIC_CHAR16_T_LOCK_FREE 2
+#define __GCC_ATOMIC_CHAR32_T_LOCK_FREE 2
+#define __GCC_ATOMIC_CHAR_LOCK_FREE 2
+#define __GCC_ATOMIC_INT_LOCK_FREE 2
+#define __GCC_ATOMIC_LLONG_LOCK_FREE 2
+#define __GCC_ATOMIC_LONG_LOCK_FREE 2
+#define __GCC_ATOMIC_POINTER_LOCK_FREE 2
+#define __GCC_ATOMIC_SHORT_LOCK_FREE 2
+#define __GCC_ATOMIC_TEST_AND_SET_TRUEVAL 1
+#define __GCC_ATOMIC_WCHAR_T_LOCK_FREE 2
+#define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1 1
+#define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2 1
+#define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4 1
+#define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8 1
+#define __GNUC_GNU_INLINE__ 1
+#define __GNUC_GNU_INLINE__ 1
+#define __GNUC_MINOR__ 9
+#define __GNUC_PATCHLEVEL__ 0
+#define __GNUC__ 4
+#define __GXX_ABI_VERSION 1002
+#define __ICC 1900
+#define __INT16_MAX__ 32767
+#define __INT16_TYPE__ short
+#define __INT32_MAX__ 2147483647
+#define __INT32_TYPE__ int
+#define __INT64_MAX__ 9223372036854775807L
+#define __INT64_TYPE__ long
+#define __INT8_MAX__ 127
+#define __INT8_TYPE__ signed char
+#define __INTEL_COMPILER 1900
+#define __INTEL_COMPILER_BUILD_DATE 20190206
+#define __INTEL_COMPILER_UPDATE 0
+#define __INTEL_RTTI__ 1
+#define __INTMAX_MAX__ 0x7fffffffffffffff
+#define __INTMAX_TYPE__ long int
+#define __INTPTR_MAX__ 9223372036854775807L
+#define __INTPTR_TYPE__ long
+#define __INT_FAST16_MAX__ 9223372036854775807L
+#define __INT_FAST16_TYPE__ long
+#define __INT_FAST32_MAX__ 9223372036854775807L
+#define __INT_FAST32_TYPE__ long
+#define __INT_FAST64_MAX__ 9223372036854775807L
+#define __INT_FAST64_TYPE__ long
+#define __INT_FAST8_MAX__ 127
+#define __INT_FAST8_TYPE__ char
+#define __INT_LEAST16_MAX__ 32767
+#define __INT_LEAST16_TYPE__ short
+#define __INT_LEAST32_MAX__ 2147483647
+#define __INT_LEAST32_TYPE__ int
+#define __INT_LEAST64_MAX__ 9223372036854775807L
+#define __INT_LEAST64_TYPE__ long
+#define __INT_LEAST8_MAX__ 127
+#define __INT_LEAST8_TYPE__ char
+#define __INT_MAX__ 2147483647
+#define __LDBL_DENORM_MIN__ 3.64519953188247460253e-4951L
+#define __LDBL_DIG__ 18
+#define __LDBL_EPSILON__ 1.08420217248550443401e-19L
+#define __LDBL_HAS_DENORM__ 1
+#define __LDBL_HAS_INFINITY__ 1
+#define __LDBL_HAS_QUIET_NAN__ 1
+#define __LDBL_MANT_DIG__ 64
+#define __LDBL_MAX_10_EXP__ 4932
+#define __LDBL_MAX_EXP__ 16384
+#define __LDBL_MAX__ 1.18973149535723176502e+4932L
+#define __LDBL_MIN_10_EXP__ -4931
+#define __LDBL_MIN_EXP__ -16381
+#define __LDBL_MIN__ 3.36210314311209350626e-4932L
+#define __LITTLE_ENDIAN__ 1
+#define __LONG_DOUBLE_SIZE__ 80
+#define __LONG_LONG_MAX__ 0x7fffffffffffffff
+#define __LONG_MAX__ 9223372036854775807L
+#define __LP64__ 1
+#define __MACH__ 1
+#define __MMX__ 1
+#define __NO_MATH_INLINES 1
+#define __NO_STRING_INLINES 1
+#define __OPTIMIZE__ 1
+#define __ORDER_BIG_ENDIAN__ 4321
+#define __ORDER_LITTLE_ENDIAN__ 1234
+#define __ORDER_PDP_ENDIAN__ 3412
+#define __PIC__ 1
+#define __PRAGMA_REDEFINE_EXTNAME 1
+#define __PTRDIFF_MAX__ 9223372036854775807L
+#define __PTRDIFF_TYPE__ long
+#define __QMSPP_ 1
+#define __REGISTER_PREFIX__ 
+#define __SCHAR_MAX__ 127
+#define __SHRT_MAX__ 32767
+#define __SIGNED_CHARS__ 1
+#define __SIG_ATOMIC_MAX__ 2147483647
+#define __SIG_ATOMIC_MIN__ (-__SIG_ATOMIC_MAX__ - 1)
+#define __SIG_ATOMIC_TYPE__ int
+#define __SIZEOF_DOUBLE__ 8
+#define __SIZEOF_FLOAT128__ 16
+#define __SIZEOF_FLOAT80__ 12
+#define __SIZEOF_FLOAT__ 4
+#define __SIZEOF_INT128__ 16
+#define __SIZEOF_INT__ 4
+#define __SIZEOF_LONG_DOUBLE__ 16
+#define __SIZEOF_LONG_LONG__ 8
+#define __SIZEOF_LONG__ 8
+#define __SIZEOF_POINTER__ 8
+#define __SIZEOF_PTRDIFF_T__ 8
+#define __SIZEOF_SHORT__ 2
+#define __SIZEOF_SIZE_T__ 8
+#define __SIZEOF_WCHAR_T__ 4
+#define __SIZEOF_WINT_T__ 4
+#define __SIZE_MAX__ 18446744073709551615UL
+#define __SIZE_TYPE__ unsigned long
+#define __SIZE_WIDTH__ 64
+#define __SSE2_MATH__ 1
+#define __SSE2__ 1
+#define __SSE3__ 1
+#define __SSE_MATH__ 1
+#define __SSE__ 1
+#define __SSSE3__ 1
+#define __STDC_DEC_FP__ 200704L
+#define __STDC_HOSTED__ 1
+#define __STDC__ 1
+#define __TIME__ "17:54:52"
+#define __UINT16_MAX__ 65535
+#define __UINT16_TYPE__ unsigned short
+#define __UINT32_MAX__ 4294967295U
+#define __UINT32_TYPE__ unsigned int
+#define __UINT64_MAX__ 18446744073709551615UL
+#define __UINT64_TYPE__ unsigned long
+#define __UINT8_MAX__ 255
+#define __UINT8_TYPE__ unsigned char
+#define __UINTMAX_MAX__ 0xffffffffffffffff
+#define __UINTMAX_TYPE__ long unsigned int
+#define __UINTPTR_MAX__ 18446744073709551615UL
+#define __UINTPTR_TYPE__ unsigned long
+#define __UINT_FAST16_MAX__ 18446744073709551615UL
+#define __UINT_FAST16_TYPE__ unsigned long
+#define __UINT_FAST32_MAX__ 18446744073709551615UL
+#define __UINT_FAST32_TYPE__ unsigned long
+#define __UINT_FAST64_MAX__ 18446744073709551615UL
+#define __UINT_FAST64_TYPE__ unsigned long
+#define __UINT_FAST8_MAX__ 255
+#define __UINT_FAST8_TYPE__ unsigned char
+#define __UINT_LEAST16_MAX__ 65535
+#define __UINT_LEAST16_TYPE__ unsigned short
+#define __UINT_LEAST32_MAX__ 4294967295U
+#define __UINT_LEAST32_TYPE__ unsigned int
+#define __UINT_LEAST64_MAX__ 18446744073709551615UL
+#define __UINT_LEAST64_TYPE__ unsigned long
+#define __UINT_LEAST8_MAX__ 255
+#define __UINT_LEAST8_TYPE__ unsigned char
+#define __USER_LABEL_PREFIX__ _
+#define __VERSION__ "Intel(R) C++ gcc 4.9 mode"
+#define __WCHAR_MAX__ 2147483647
+#define __WCHAR_MIN__ (-__WCHAR_MAX__ - 1)
+#define __WCHAR_TYPE__ int
+#define __WINT_MAX__ 4294967295U
+#define __WINT_MIN__ 0U
+#define __WINT_TYPE__ int
+#define __amd64 1
+#define __amd64__ 1
+#define __cilk 200
+#define __clang__ 1
+#define __clang__ 1
+#define __clang_major__ 10
+#define __clang_major__ 4
+#define __clang_minor__ 0
+#define __clang_minor__ 0
+#define __clang_patchlevel__ 0
+#define __clang_patchlevel__ 1
+#define __clang_version__ "10.0.1"
+#define __pentium4 1
+#define __pentium4__ 1
+#define __private_extern__ __attribute__((visibility("hidden")))
+#define __tune_pentium4__ 1
+#define __x86_64 1
+#define __x86_64__ 1
