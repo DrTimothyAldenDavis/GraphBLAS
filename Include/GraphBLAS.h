60879f55dca83c54573643d282bc8dd9b4857e0b
Tim Davis <DrTimothyAldenDavis@gmail.com>
2020-08-08 22:34:31 -0500

    Aug8.draft4

--- Include/GraphBLAS.h
+++ Include/GraphBLAS.h
@@ -112,7 +112,7 @@
 
 // The version of this implementation, and the GraphBLAS API version:
 #define GxB_IMPLEMENTATION_NAME "SuiteSparse:GraphBLAS"
-#define GxB_IMPLEMENTATION_DATE "Aug 5, 2020 (DRAFT)"
+#define GxB_IMPLEMENTATION_DATE "Aug 8, 2020 (DRAFT4)"
 #define GxB_IMPLEMENTATION_MAJOR 4
 #define GxB_IMPLEMENTATION_MINOR 0
 #define GxB_IMPLEMENTATION_SUB   0
