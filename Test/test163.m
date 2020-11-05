71808a249d0df47f3ae7073b939d80182ec1753e
Tim Davis <DrTimothyAldenDavis@gmail.com>
2020-11-05 09:48:19 -0600

    tests

--- Test/test163.m
+++ Test/test163.m
@@ -4,7 +4,7 @@ function test163
 % SuiteSparse:GraphBLAS, Timothy A. Davis, (c) 2017-2020, All Rights Reserved.
 % http://suitesparse.com   See GraphBLAS/Doc/License.txt for license.
 
-GrB.burble (1)
+GrB.burble (1) ;
 semiring.add = 'plus' ;
 semiring.multiply = 'times' ;
 semiring.class = 'double' ;
@@ -13,12 +13,8 @@ rng ('default') ;
 
 n = 1000 ;
 m = 10 ;
-% n = 3 ;
-% m = 3 ;
 A = sprand (m, n, 0.1) ;
-% A = sparse (rand (m,n)) ;
 B = sprand (m, n, 0.1) ;
-% B = sparse (rand (m,n)) ;
 M = logical (sprand (n, n, 0.1)) ;
 Cin = sparse (n, n) ;
 dtn = struct ('inp0', 'tran', 'mask', 'complement', 'axb', 'dot') ;
@@ -27,4 +23,4 @@ C1 = double (~M) .* (A'*B) ;
 C2 = GB_mex_mxm (Cin, M, [ ], semiring, A, B, dtn) ;
 GB_spec_compare (C1, C2) ;
 
-GrB.burble (0)
+GrB.burble (0) ;
