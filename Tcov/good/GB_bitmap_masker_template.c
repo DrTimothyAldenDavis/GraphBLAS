0c7050aa270e80ff3a4c77752583ff1fa655ca7f
Tim Davis <davis@tamu.edu>
2020-11-06 21:49:20 -0600

    tcoverage

---
+++ Tcov/good/GB_bitmap_masker_template.c
@@ -0,0 +1,303 @@
+//------------------------------------------------------------------------------
+// GB_bitmap_masker_template:  phase2 for R = masker (C, M, Z), R is bitmap
+//------------------------------------------------------------------------------
+
+// SuiteSparse:GraphBLAS, Timothy A. Davis, (c) 2017-2020, All Rights Reserved.
+// http://suitesparse.com   See GraphBLAS/Doc/License.txt for license.
+
+//------------------------------------------------------------------------------
+
+// Computes C<M>=Z or C<!M>=Z, returning the result in R, which is bitmap.
+// The input matrix C is not modified.  Effectively, this computes R=C and then
+// R<M>=Z or R<!M>=Z.  If the C_replace descriptor is enabled, then C has
+// already been cleared, and is an empty (but non-NULL) matrix.
+
+// phase2: computes R in a single pass
+
+// C is sparse or hypersparse.  Z is bitmap or full.  R is bitmap.
+// M has any sparsity structure.
+
+        //      ------------------------------------------
+        //      C       <!M> =       Z              R
+        //      ------------------------------------------
+
+        //      sparse  sparse      bitmap          bitmap
+        //      sparse  sparse      full            bitmap
+
+        //      sparse  bitmap      bitmap          bitmap
+        //      sparse  bitmap      full            bitmap
+
+        //      sparse  full        bitmap          bitmap
+        //      sparse  full        full            bitmap
+
+        //      ------------------------------------------
+        //      C       <M> =        Z              R
+        //      ------------------------------------------
+
+        //      sparse  bitmap      bitmap          bitmap
+        //      sparse  bitmap      full            bitmap
+
+        //      sparse  full        bitmap          bitmap
+        //      sparse  full        full            bitmap
+
+// FUTURE:: add special cases for C==Z, C==M, and Z==M aliases
+
+{
+    
+    int64_t p, rnvals = 0 ;
+
+    ASSERT (R_sparsity == GxB_BITMAP) ;
+    ASSERT (C_is_sparse || C_is_hyper) ;
+    ASSERT (Z_is_bitmap || Z_is_full) ;
+
+    //--------------------------------------------------------------------------
+    // scatter C into the R bitmap
+    //--------------------------------------------------------------------------
+
+    ASSERT_MATRIX_OK (C, "C input to R_bitmap_masker", GB0) ;
+    GB_SLICE_MATRIX (C, 8) ;
+
+    #pragma omp parallel for num_threads(C_nthreads) schedule(dynamic,1) \
+        reduction(+:rnvals)
+    for (taskid = 0 ; taskid < C_ntasks ; taskid++)
+    {
+        int64_t kfirst = kfirst_Cslice [taskid] ;
+        int64_t klast  = klast_Cslice  [taskid] ;
+        for (int64_t k = kfirst ; k <= klast ; k++)
+        {
+            // find the part of C(:,k) for this task
+            int64_t j = GBH (Ch, k) ;
+            int64_t pC_start, pC_end ;
+            GB_get_pA (&pC_start, &pC_end, taskid, k, kfirst,
+                klast, pstart_Cslice, Cp, vlen) ;
+            int64_t pR_start = j * vlen ;
+            // traverse over C(:,j), the kth vector of C
+            for (int64_t pC = pC_start ; pC < pC_end ; pC++)
+            {   GB_cov[1143]++ ;
+// covered (1143): 66
+                // R(i,j) = C(i,j)
+                int64_t i = Ci [pC] ;
+                int64_t pR = pR_start + i ;
+                Rb [pR] = 1 ;
+                rnvals++ ;
+                memcpy (Rx + (pR)*rsize, Cx +(pC)*rsize, rsize) ;
+            }
+        }
+    }
+
+    R->nvals = rnvals ;
+    ASSERT_MATRIX_OK (R, "R with C scattered", GB0) ;
+
+    //--------------------------------------------------------------------------
+    // R<M>=Z or R<!M>=Z
+    //--------------------------------------------------------------------------
+
+    if (M_is_sparse || M_is_hyper)
+    {
+
+        //----------------------------------------------------------------------
+        // Method05: M is sparse or hypersparse, Z bitmap/full, R bitmap
+        //----------------------------------------------------------------------
+
+        //      ------------------------------------------
+        //      C       <!M> =       Z              R
+        //      ------------------------------------------
+
+        //      sparse  sparse      bitmap          bitmap
+        //      sparse  sparse      full            bitmap
+
+        ASSERT (Mask_comp) ;
+
+        //----------------------------------------------------------------------
+        // scatter M into the R bitmap
+        //----------------------------------------------------------------------
+
+        GB_SLICE_MATRIX (M, 8) ;
+
+        #pragma omp parallel for num_threads(M_nthreads) schedule(dynamic,1)
+        for (taskid = 0 ; taskid < M_ntasks ; taskid++)
+        {
+            int64_t kfirst = kfirst_Mslice [taskid] ;
+            int64_t klast  = klast_Mslice  [taskid] ;
+            for (int64_t k = kfirst ; k <= klast ; k++)
+            {
+                // find the part of M(:,k) for this task
+                int64_t j = GBH (Mh, k) ;
+                int64_t pM_start, pM_end ;
+                GB_get_pA (&pM_start, &pM_end, taskid, k, kfirst,
+                    klast, pstart_Mslice, Mp, vlen) ;
+                int64_t pR_start = j * vlen ;
+                // traverse over M(:,j), the kth vector of M
+                for (int64_t pM = pM_start ; pM < pM_end ; pM++)
+                {
+                    // mark R(i,j) if M(i,j) is true
+                    bool mij = GB_mcast (Mx, pM, msize) ;
+                    if (mij)
+                    {   GB_cov[1144]++ ;
+// covered (1144): 48
+                        int64_t i = Mi [pM] ;
+                        int64_t p = pR_start + i ;
+                        Rb [p] += 2 ;
+                    }
+                }
+            }
+        }
+
+        //----------------------------------------------------------------------
+        // R<!M>=Z, using M scattered into R
+        //----------------------------------------------------------------------
+
+        // Rb is marked as follows:
+        //  0:  R(i,j) is not present, and M(i,j) is false
+        //  1:  R(i,j) is present, and M(i,j) is false
+        //  2:  R(i,j) is not present, and M(i,j) is true
+        //  3:  R(i,j) is present, and M(i,j) is true
+
+        // M is complemented, but shown uncomplemented in the table below since
+        // that is how it is scattered into R.
+
+        // Rb   R(i,j)  M(i,j)  Z(i,j)      modification to R(i,j)
+        // 0    -       0       zij         R(i,j) = Z(i,j), new value, rnvals++
+        // 0    -       0       -           do nothing
+        // 1    rij     0       zij         R(i,j) = Z(i,j), overwrite
+        // 1    rij     0       -           delete R(i,j), rnvals--
+        // 2    -       1       zij         do nothing, set Rb to 0
+        // 2    -       1       -           do nothing, set Rb to 0
+        // 3    rij     1       zij         keep R(i,j), set Rb to 1
+        // 3    rij     1       -           keep R(i,j), set Rb to 1
+
+        #pragma omp parallel for num_threads(R_nthreads) schedule(static) \
+            reduction(+:rnvals)
+        for (p = 0 ; p < rnz ; p++)
+        {
+            int8_t r = Rb [p] ;
+            int8_t z = GBB (Zb, p) ;
+            switch (r)
+            {
+                case 0  : GB_cov[1145]++ ;     // R(i,j) not present, M(i,j) false
+// covered (1145): 178
+                    if (z)
+                    {   GB_cov[1146]++ ;
+// covered (1146): 87
+                        // R(i,j) = Z(i,j), insert new value
+                        memcpy (Rx +(p)*rsize, Zx +(p)*rsize, rsize) ;
+                        Rb [p] = 1 ;
+                        rnvals++ ;
+                    }
+                    break ;
+
+                case 1  : GB_cov[1147]++ ;     // R(i,j) present, M(i,j) false
+// covered (1147): 54
+                    if (z)
+                    {   GB_cov[1148]++ ;
+// covered (1148): 27
+                        // R(i,j) = Z(i,j), update prior value
+                        memcpy (Rx +(p)*rsize, Zx +(p)*rsize, rsize) ;
+                    }
+                    else
+                    {   GB_cov[1149]++ ;
+// covered (1149): 27
+                        // delete R(i,j)
+                        Rb [p] = 0 ;
+                        rnvals-- ;
+                    }
+                    break ;
+
+                case 2  : GB_cov[1150]++ ;     // R(i,j) not present, M(i,j) true
+// covered (1150): 41
+                    Rb [p] = 0 ;
+                    break ;
+
+                case 3  : GB_cov[1151]++ ;     // R(i,j) present, M(i,j) true
+// covered (1151): 13
+                    Rb [p] = 1 ;
+                    break ;
+
+                default: ;
+            }
+        }
+
+    }
+    else
+    {   GB_cov[1152]++ ;
+// covered (1152): 1311
+
+        //----------------------------------------------------------------------
+        // Method06: M and Z are bitmap or full, R is bitmap
+        //----------------------------------------------------------------------
+
+        //      ------------------------------------------
+        //      C       <!M> =       Z              R
+        //      ------------------------------------------
+
+        //      sparse  bitmap      bitmap          bitmap
+        //      sparse  bitmap      full            bitmap
+
+        //      sparse  full        bitmap          bitmap
+        //      sparse  full        full            bitmap
+
+        //      ------------------------------------------
+        //      C       <M> =        Z              R
+        //      ------------------------------------------
+
+        //      sparse  bitmap      bitmap          bitmap
+        //      sparse  bitmap      full            bitmap
+
+        //      sparse  full        bitmap          bitmap
+        //      sparse  full        full            bitmap
+
+        // Rb   R(i,j)  M(i,j)  Z(i,j)      modification to R(i,j)
+
+        // 0    -       0       zij         do nothing
+        // 0    -       0       -           do nothing
+        // 1    rij     0       zij         do nothing
+        // 1    rij     0       -           do nothing
+
+        // 0    -       1       zij         R(i,j) = Z(i,j), rnvals++
+        // 0    -       1       -           do nothing
+        // 1    rij     1       zij         R(i,j) = Z(i,j), no change to rnvals
+        // 1    rij     1       -           delete, rnvals--
+
+// TODO#pragma omp parallel for num_threads(R_nthreads) schedule(static) \
+// TODO     reduction(+:rnvals)
+        for (p = 0 ; p < rnz ; p++)
+        {
+            bool mij = GBB (Mb, p) && GB_mcast (Mx, p, msize) ;
+            if (Mask_comp) mij = !mij ;
+            if (mij)
+            {
+                int8_t z = GBB (Zb, p) ;
+                int8_t r = Rb [p] ;
+                if (r)
+                {
+                    if (z)
+                    {   GB_cov[1153]++ ;
+// NOT COVERED (1153):
+GB_GOTCHA ; // masker with R bitmap, C sparse, M/Z bitmap/full
+                        // R(i,j) = Z(i,j), update, no change to rnvals
+                        memcpy (Rx +(p)*rsize, Zx +(p)*rsize, rsize) ;
+                    }
+                    else
+                    {   GB_cov[1154]++ ;
+// NOT COVERED (1154):
+GB_GOTCHA ; // masker with R bitmap, C sparse, M/Z bitmap/full  (LARGE) ]
+                        // delete R(i,j)
+                        Rb [p] = 0 ;
+                        rnvals-- ;
+                    }
+                }
+                else if (z)
+                {   GB_cov[1155]++ ;
+// covered (1155): 5825
+                    // R(i,j) = Z(i,j), new entry
+                    memcpy (Rx +(p)*rsize, Zx +(p)*rsize, rsize) ;
+                    Rb [p] = 1 ;
+                    rnvals++ ;
+                }
+            }
+        }
+    }
+
+    R->nvals = rnvals ;
+}
+
---
+++ Tcov/good/GB_dense_subassign_06d_template.c
@@ -0,0 +1,333 @@
+//------------------------------------------------------------------------------
+// GB_dense_subassign_06d_template: C<A> = A where C is dense or bitmap
+//------------------------------------------------------------------------------
+
+// SuiteSparse:GraphBLAS, Timothy A. Davis, (c) 2017-2020, All Rights Reserved.
+// http://suitesparse.com   See GraphBLAS/Doc/License.txt for license.
+
+//------------------------------------------------------------------------------
+
+{
+
+    //--------------------------------------------------------------------------
+    // get C and A
+    //--------------------------------------------------------------------------
+
+    ASSERT (!GB_ZOMBIES (A)) ;
+    ASSERT (GB_JUMBLED_OK (A)) ;
+    ASSERT (!GB_PENDING (A)) ;
+
+    const int64_t  *GB_RESTRICT Ap = A->p ;
+    const int64_t  *GB_RESTRICT Ah = A->h ;
+    const int64_t  *GB_RESTRICT Ai = A->i ;
+    const int8_t   *GB_RESTRICT Ab = A->b ;
+    const GB_ATYPE *GB_RESTRICT Ax = (GB_ATYPE *) A->x ;
+    const int64_t avlen = A->vlen ;
+    const bool A_is_bitmap = GB_IS_BITMAP (A) ;
+    const bool A_is_dense = GB_as_if_full (A) ;
+    const int64_t anz = GB_NNZ_HELD (A) ;
+
+    GB_CTYPE *GB_RESTRICT Cx = (GB_CTYPE *) C->x ;
+    int8_t   *GB_RESTRICT Cb = C->b ;
+    const int64_t cvlen = C->vlen ;
+    const bool C_is_bitmap = GB_IS_BITMAP (C) ;
+
+    //--------------------------------------------------------------------------
+    // C<A> = A
+    //--------------------------------------------------------------------------
+
+    int64_t p ;
+    int64_t cnvals = C->nvals ;     // for C bitmap
+
+    if (A_is_dense)
+    {   GB_cov[1168]++ ;
+// covered (1168): 2929
+
+        //----------------------------------------------------------------------
+        // A is dense: all entries present
+        //----------------------------------------------------------------------
+
+        if (C_is_bitmap)
+        {   GB_cov[1169]++ ;
+// covered (1169): 780
+
+            //------------------------------------------------------------------
+            // C is bitmap, A is dense
+            //------------------------------------------------------------------
+
+            if (Mask_struct)
+            {
+                // C<A,struct>=A with C bitmap, A dense
+                #pragma omp parallel for num_threads(nthreads) schedule(static)
+                for (p = 0 ; p < anz ; p++)
+                {   GB_cov[1170]++ ;
+// NOT COVERED (1170):
+                    // Cx [p] = Ax [p]
+                    GB_COPY_A_TO_C (Cx, p, Ax, p) ;
+                    Cb [p] = 1 ;
+                }
+                cnvals = anz ;
+            }
+            else
+            {
+                // C<A>=A with C bitmap, A dense
+                #pragma omp parallel for num_threads(nthreads) schedule(static)\
+                    reduction(+:cnvals)
+                for (p = 0 ; p < anz ; p++)
+                {
+                    if (GB_AX_MASK (Ax, p, asize))
+                    {   GB_cov[1171]++ ;
+// covered (1171): 23568
+                        // Cx [p] = Ax [p]
+                        GB_COPY_A_TO_C (Cx, p, Ax, p) ;
+                        cnvals += (Cb [p] == 0) ;
+                        Cb [p] = 1 ;
+                    }
+                }
+            }
+
+        }
+        else
+        {
+
+            //------------------------------------------------------------------
+            // C is hypersparse, sparse, or full, with all entries present
+            //------------------------------------------------------------------
+
+            if (Mask_struct)
+            {
+                // C<A,struct>=A with C sparse/hyper/full
+                #pragma omp parallel for num_threads(nthreads) schedule(static)
+                for (p = 0 ; p < anz ; p++)
+                {   GB_cov[1172]++ ;
+// NOT COVERED (1172):
+                    // Cx [p] = Ax [p]
+                    GB_COPY_A_TO_C (Cx, p, Ax, p) ;
+                }
+            }
+            else
+            {
+                // C<A>=A with C sparse/hyper/full
+                #pragma omp parallel for num_threads(nthreads) schedule(static)
+                for (p = 0 ; p < anz ; p++)
+                {
+                    if (GB_AX_MASK (Ax, p, asize))
+                    {   GB_cov[1173]++ ;
+// covered (1173): 71797
+                        // Cx [p] = Ax [p]
+                        GB_COPY_A_TO_C (Cx, p, Ax, p) ;
+                    }
+                }
+            }
+        }
+
+    }
+    else if (A_is_bitmap)
+    {
+
+        //----------------------------------------------------------------------
+        // A is bitmap
+        //----------------------------------------------------------------------
+
+        if (C_is_bitmap)
+        {
+
+            //------------------------------------------------------------------
+            // C is bitmap, A is bitmap
+            //------------------------------------------------------------------
+
+            if (Mask_struct)
+            {
+                // C<A,struct>=A with A and C bitmap
+                #pragma omp parallel for num_threads(nthreads) schedule(static)\
+                    reduction(+:cnvals)
+                for (p = 0 ; p < anz ; p++)
+                {
+                    if (Ab [p])
+                    {   GB_cov[1174]++ ;
+// covered (1174): 1105
+                        // Cx [p] = Ax [p]
+                        GB_COPY_A_TO_C (Cx, p, Ax, p) ;
+                        cnvals += (Cb [p] == 0) ;
+                        Cb [p] = 1 ;
+                    }
+                }
+            }
+            else
+            {
+                // C<A>=A with A and C bitmap
+                #pragma omp parallel for num_threads(nthreads) schedule(static)\
+                    reduction(+:cnvals)
+                for (p = 0 ; p < anz ; p++)
+                {
+                    if (Ab [p] && GB_AX_MASK (Ax, p, asize))
+                    {   GB_cov[1175]++ ;
+// NOT COVERED (1175):
+                        // Cx [p] = Ax [p]
+                        GB_COPY_A_TO_C (Cx, p, Ax, p) ;
+                        cnvals += (Cb [p] == 0) ;
+                        Cb [p] = 1 ;
+                    }
+                }
+            }
+
+        }
+        else
+        {
+
+            //------------------------------------------------------------------
+            // C is hypersparse, sparse, or full, with all entries present
+            //------------------------------------------------------------------
+
+            if (Mask_struct)
+            {
+                // C<A,struct>=A with A bitmap, and C hyper/sparse/full
+                // this method is used by LAGraph_bfs_parent when q is
+                // a bitmap and pi is full.
+                #pragma omp parallel for num_threads(nthreads) schedule(static)
+                for (p = 0 ; p < anz ; p++)
+                {
+                    // Cx [p] = Ax [p]
+                    if (Ab [p])
+                    {   GB_cov[1176]++ ;
+// covered (1176): 3392
+                        GB_COPY_A_TO_C (Cx, p, Ax, p) ;
+                    }
+                }
+            }
+            else
+            {
+                // C<A>=A with A bitmap, and C hyper/sparse/full
+                #pragma omp parallel for num_threads(nthreads) schedule(static)
+                for (p = 0 ; p < anz ; p++)
+                {
+                    if (Ab [p] && GB_AX_MASK (Ax, p, asize))
+                    {   GB_cov[1177]++ ;
+// NOT COVERED (1177):
+                        // Cx [p] = Ax [p]
+                        GB_COPY_A_TO_C (Cx, p, Ax, p) ;
+                    }
+                }
+            }
+        }
+
+    }
+    else
+    {
+
+        //----------------------------------------------------------------------
+        // A is hypersparse or sparse; C is dense or a bitmap
+        //----------------------------------------------------------------------
+
+        int taskid ;
+        #pragma omp parallel for num_threads(nthreads) schedule(dynamic,1) \
+            reduction(+:cnvals)
+        for (taskid = 0 ; taskid < ntasks ; taskid++)
+        {
+
+            // if kfirst > klast then taskid does no work at all
+            int64_t kfirst = kfirst_slice [taskid] ;
+            int64_t klast  = klast_slice  [taskid] ;
+
+            //------------------------------------------------------------------
+            // C<A(:,kfirst:klast)> = A(:,kfirst:klast)
+            //------------------------------------------------------------------
+
+            for (int64_t k = kfirst ; k <= klast ; k++)
+            {
+
+                //--------------------------------------------------------------
+                // find the part of A(:,k) to be operated on by this task
+                //--------------------------------------------------------------
+
+                int64_t j = GBH (Ah, k) ;
+                int64_t pA_start, pA_end ;
+                GB_get_pA (&pA_start, &pA_end, taskid, k,
+                    kfirst, klast, pstart_slice, Ap, avlen) ;
+
+                // pC points to the start of C(:,j) if C is dense or bitmap
+                int64_t pC = j * cvlen ;
+
+                //--------------------------------------------------------------
+                // C<A(:,j)> = A(:,j)
+                //--------------------------------------------------------------
+
+                if (Mask_struct)
+                {
+                    if (C_is_bitmap)
+                    {
+                        // C<A,struct>=A with C bitmap, A sparse
+                        GB_PRAGMA_SIMD_VECTORIZE
+                        for (int64_t pA = pA_start ; pA < pA_end ; pA++)
+                        {   GB_cov[1178]++ ;
+// covered (1178): 746
+                            int64_t p = pC + GBI (Ai, pA, avlen) ;
+                            // Cx [p] = Ax [pA]
+                            GB_COPY_A_TO_C (Cx, p, Ax, pA) ;
+                            cnvals += (Cb [p] == 0) ;
+                            Cb [p] = 1 ;
+                        }
+                    }
+                    else
+                    {
+                        // C<A,struct>=A with C full, A sparse
+                        GB_PRAGMA_SIMD_VECTORIZE
+                        for (int64_t pA = pA_start ; pA < pA_end ; pA++)
+                        {   GB_cov[1179]++ ;
+// covered (1179): 3410
+                            int64_t p = pC + GBI (Ai, pA, avlen) ;
+                            // Cx [p] = Ax [pA]
+                            GB_COPY_A_TO_C (Cx, p, Ax, pA) ;
+                        }
+                    }
+                }
+                else
+                {
+                    if (C_is_bitmap)
+                    {
+                        // C<A,struct>=A with C bitmap, A sparse
+                        GB_PRAGMA_SIMD_VECTORIZE
+                        for (int64_t pA = pA_start ; pA < pA_end ; pA++)
+                        {
+                            if (GB_AX_MASK (Ax, pA, asize))
+                            {   GB_cov[1180]++ ;
+// NOT COVERED (1180):
+                                int64_t p = pC + GBI (Ai, pA, avlen) ;
+                                // Cx [p] = Ax [pA]
+                                GB_COPY_A_TO_C (Cx, p, Ax, pA) ;
+                                cnvals += (Cb [p] == 0) ;
+                                Cb [p] = 1 ;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // C<A,struct>=A with C dense, A sparse
+                        GB_PRAGMA_SIMD_VECTORIZE
+                        for (int64_t pA = pA_start ; pA < pA_end ; pA++)
+                        {
+                            if (GB_AX_MASK (Ax, pA, asize))
+                            {   GB_cov[1181]++ ;
+// NOT COVERED (1181):
+                                int64_t p = pC + GBI (Ai, pA, avlen) ;
+                                // Cx [p] = Ax [pA]
+                                GB_COPY_A_TO_C (Cx, p, Ax, pA) ;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    // log the number of entries in the C bitmap
+    //--------------------------------------------------------------------------
+
+    if (C_is_bitmap)
+    {   GB_cov[1182]++ ;
+// covered (1182): 840
+        C->nvals = cnvals ;
+    }
+}
+
