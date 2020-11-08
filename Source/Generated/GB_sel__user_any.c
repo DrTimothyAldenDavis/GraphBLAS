391adcc8eccab2d61c6f9967222fa69817f29487
Tim Davis <davis@tamu.edu>
2020-11-08 08:01:14 -0600

    user select tested by gbtest42

--- Source/Generated/GB_sel__user_any.c
+++ Source/Generated/GB_sel__user_any.c
@@ -113,7 +113,7 @@ void GB_sel_bitmap__user_any
     const int nthreads
 )
 { 
-GB_GOTCHA ; // GB_sel_bitmap__user_any
+// GB_GOTCHA ; // GB_sel_bitmap__user_any: gbtest41
     ;
     #include "GB_bitmap_select_template.c"
 }
