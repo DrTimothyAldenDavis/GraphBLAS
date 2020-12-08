faad7f3101d7ae113085b636c316b668e0894ca5
Tim Davis <DrTimothyAldenDavis@gmail.com>
2020-12-08 15:10:18 -0600

    dot2 rule: tweak "C huge" case and coldeg 1400->1024"

--- Source/GB_AxB_dot2_control.c
+++ Source/GB_AxB_dot2_control.c
@@ -10,9 +10,6 @@
 // C=A'*B, C<M>=A'*B, or C<!M>=A'*B where C is constructed in bitmap format.
 // C must be small and likely very dense.
 
-// TODO:: tune this heuristic.  See gbtest99 and k = 1000.
-// See also SuiteSparse/MATLAB_Tools/SSMULT/ssmult.c
-
 #include "GB_mxm.h"
 
 bool GB_AxB_dot2_control  // true: use dot2, false: use saxpy
@@ -63,18 +60,18 @@ bool GB_AxB_dot2_control  // true: use dot2, false: use saxpy
 
     double row_degree = anz / avlen ;
     double col_degree = anz / anvec ;
-    GBURBLE ("(rowdeg %g coldeg %g) ", row_degree, col_degree) ; // TODO::remove
+    GBURBLE ("(rowdeg %g coldeg %g ", row_degree, col_degree) ; // TODO::remove
+    GBURBLE ("anz+bnz %g cnz %g) ", anz+bnz, cnz) ; // TODO::remove
 
     double cnz = (anvec * bnvec) ;  // size of the C bitmap
 
-    if (anz + bnz < 10 * cnz)
+    if (cnz > anz + bnz)
     { 
         // The C bitmap is too big, use saxpy and construct C as sparse
         GBURBLE ("(C huge: saxpy C=(A')*B) ") ;
         return (false) ;
     }
 
-    GBURBLE ("(anz+bnz %g cnz %g) ", anz+bnz, cnz) ; // TODO::remove
     if ((anz + bnz > 10000 * cnz) || (cnz <= 100))
     { 
         // The C bitmap is very small compared with A and B, so use dot2
@@ -84,7 +81,7 @@ bool GB_AxB_dot2_control  // true: use dot2, false: use saxpy
     }
 
     // average # of entries in each row and column of A (assuming A is CSC)
-    if (row_degree < 0.125 && col_degree > 1400)
+    if (row_degree < 0.125 && col_degree > 1024)
     { 
         // If AT=A' is computed, it will have mostly empty vectors (the
         // row_degree of A), so do not transpose it.  If the fraction of
