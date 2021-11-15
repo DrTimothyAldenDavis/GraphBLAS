2d8147cc8b0ed6a4fdd04d543e224ae9c6b5763d
Tim Davis <DrTimothyAldenDavis@gmail.com>
2021-11-15 14:33:58 -0600

    v5.2.0 stable release

--- Include/GraphBLAS.h
+++ Include/GraphBLAS.h
@@ -205,11 +205,11 @@
 
 // The version of this implementation, and the GraphBLAS API version:
 #define GxB_IMPLEMENTATION_NAME "SuiteSparse:GraphBLAS"
-#define GxB_IMPLEMENTATION_DATE "Nov 16, 2021"
+#define GxB_IMPLEMENTATION_DATE "Nov 15, 2021"
 #define GxB_IMPLEMENTATION_MAJOR 6
 #define GxB_IMPLEMENTATION_MINOR 0
 #define GxB_IMPLEMENTATION_SUB   0
-#define GxB_SPEC_DATE "Nov 16, 2021"
+#define GxB_SPEC_DATE "Nov 15, 2021"
 #define GxB_SPEC_MAJOR 2
 #define GxB_SPEC_MINOR 0
 #define GxB_SPEC_SUB   0
