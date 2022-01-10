e05adbcb60050b76440d0caee58ec0c89eaf3d2f
Tim Davis <DrTimothyAldenDavis@gmail.com>
2022-01-09 18:21:08 -0600

    update user guide to reflect icx performance

--- Include/GraphBLAS.h
+++ Include/GraphBLAS.h
@@ -206,7 +206,7 @@
 
 // The version of this implementation, and the GraphBLAS API version:
 #define GxB_IMPLEMENTATION_NAME "SuiteSparse:GraphBLAS"
-#define GxB_IMPLEMENTATION_DATE "Jan 6, 2022"
+#define GxB_IMPLEMENTATION_DATE "Jan 9, 2022"
 #define GxB_IMPLEMENTATION_MAJOR 6
 #define GxB_IMPLEMENTATION_MINOR 1
 #define GxB_IMPLEMENTATION_SUB   4
