f7aefe47029dae29a64a931bca82b27e3e20ee31
Corey J. Nolet <cjnolet@gmail.com>
2022-06-23 19:26:50 -0400

    Rename sr_code -> rcode in reduce kernel

--- CUDA/templates/GB_jit_reduceNonZombiesWarp.cuh
+++ CUDA/templates/GB_jit_reduceNonZombiesWarp.cuh
@@ -25,7 +25,7 @@
 
 using namespace cooperative_groups;
 
-template< typename T, int tile_sz, int sr_code>
+template< typename T, int tile_sz, int rcode>
 __inline__ __device__ 
 T warp_ReduceSum( thread_block_tile<tile_sz> g, T val)
 {
@@ -54,7 +54,7 @@ T warp_ReduceSum( thread_block_tile<tile_sz> g, T val)
 }
 
 
-template<typename T, int warpSize, int sr_code>
+template<typename T, int warpSize, int rcode>
 __inline__ __device__
 T block_ReduceSum(thread_block g, T val)
 {
@@ -64,7 +64,7 @@ T block_ReduceSum(thread_block g, T val)
   thread_block_tile<warpSize> tile = tiled_partition<warpSize>( g );
 
   // Each warp performs partial reduction
-  val = warp_ReduceSum<T, warpSize, sr_code>( tile, val);
+  val = warp_ReduceSum<T, warpSize, rcode>( tile, val);
 
   // Wait for all partial reductions
   if (lane==0) { 
@@ -79,14 +79,14 @@ T block_ReduceSum(thread_block g, T val)
   //else { 
     val = (threadIdx.x < (blockDim.x / warpSize) ) ? shared[lane] : GB_IDENTITY ;
     //if (lane < (blockDim.x/ warpSize) ) printf("thd%d warp%d loaded val = %d\n", threadIdx.x, lane, val);
-    val = warp_ReduceSum<T, warpSize, sr_code>( tile, val); //Final reduce within first warp
+    val = warp_ReduceSum<T, warpSize, rcode>( tile, val); //Final reduce within first warp
   //}
 
   return val;
 }
 
 
-template< typename T, typename Accum, int sr_code, bool atomic_reduce = true>
+template< typename T, typename Accum, int rcode, bool atomic_reduce = true>
 __global__ void reduceNonZombiesWarp
 (
     GrB_Matrix A,
@@ -120,7 +120,7 @@ __global__ void reduceNonZombiesWarp
     // reduce work [0..s-1] to a single scalar
     //--------------------------------------------------------------------------
     // this assumes blockDim is a multiple of 32
-    sum = block_ReduceSum< T, 32, sr_code >( this_thread_block(), sum) ;
+    sum = block_ReduceSum< T, 32, rcode >( this_thread_block(), sum) ;
     this_thread_block().sync(); 
 
     // write result for this block to global mem
