7d9e8f5f978aa868c60b3ad9ad51db6e640b9f30
Tim Davis <DrTimothyAldenDavis@gmail.com>
2022-08-04 20:23:12 -0500

    testing

--- Test/GB_mex_about9.c
+++ Test/GB_mex_about9.c
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// GB_mex_about9: still more basic tests
+// GB_mex_about9: still more basic tests (not for Windows)
 //------------------------------------------------------------------------------
 
 // SuiteSparse:GraphBLAS, Timothy A. Davis, (c) 2017-2022, All Rights Reserved.
@@ -7,7 +7,7 @@
 
 //------------------------------------------------------------------------------
 
-// Test a particular case of GrB_mxm for 0-by-0 iso full matrices.
+// Windows is limited to user-defined types of size 128 or less.
 
 #include "GB_mex.h"
 #include "GB_mex_errors.h"
@@ -39,8 +39,6 @@ void mexFunction
     //--------------------------------------------------------------------------
 
     bool malloc_debug = GB_mx_get_global (true) ;
-    int expected = GrB_SUCCESS ;
-    GrB_Matrix C = NULL ;
 
     //--------------------------------------------------------------------------
     // user-defined type of 256 bytes
@@ -52,22 +50,6 @@ void mexFunction
     OK (GxB_Type_fprint (BigType, "(256-byte big type)", GxB_COMPLETE,
         stdout)) ;
     OK (GrB_Type_free (&BigType)) ;
-
-    //--------------------------------------------------------------------------
-    // reshape error handling
-    //--------------------------------------------------------------------------
-
-    GrB_Index n =  (1L << 40) ;
-    OK (GrB_Matrix_new (&C, GrB_BOOL, n, n)) ;
-    expected = GrB_OUT_OF_MEMORY ;
-    ERR (GxB_Matrix_reshape (C, true, n/2, 2*n, NULL)) ;
-    OK (GrB_Matrix_free (&C)) ;
-
-    n = 12 ;
-    OK (GrB_Matrix_new (&C, GrB_BOOL, n, n)) ;
-    expected = GrB_DIMENSION_MISMATCH ;
-    ERR (GxB_Matrix_reshape (C, true, n, 2*n, NULL)) ;
-    OK (GrB_Matrix_free (&C)) ;
 
     //--------------------------------------------------------------------------
     // wrapup
