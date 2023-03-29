faadd684b354ca11e41fc954ef76b5de1e9bdf82
Tim Davis <DrTimothyAldenDavis@gmail.com>
2023-03-29 15:08:47 -0500

    encodify mxm

--- Source/Shared/GB_config.h
+++ Source/Shared/GB_config.h
@@ -13,11 +13,11 @@
 #define GB_CONFIG_H
 
 // GB_C_COMPILER: the C compiler used to compile GraphBLAS:
-#define GB_C_COMPILER   "/usr/bin/clang"
+#define GB_C_COMPILER   "/usr/bin/gcc"
 
 // GB_C_FLAGS: the C compiler flags used to compile GraphBLAS.  Used
 // for compiling and linking:
-#define GB_C_FLAGS      " -Wno-pointer-sign  -O3 -DNDEBUG -fopenmp=libomp  -fPIC "
+#define GB_C_FLAGS      " -std=c11 -lm -Wno-pragmas  -fexcess-precision=fast  -fcx-limited-range  -fno-math-errno  -fwrapv  -O3 -DNDEBUG -fopenmp  -fPIC "
 
 // GB_C_LINK_FLAGS: the flags passed to the C compiler for the link phase:
 #define GB_C_LINK_FLAGS " -shared "
@@ -30,16 +30,16 @@
 
 // GB_SOURCE_PATH: the source code for GraphBLAS, which is the path of the
 // top-level GraphBLAS folder:
-#define GB_SOURCE_PATH  "/home/faculty/d/davis/master/GraphBLAS"
+#define GB_SOURCE_PATH  "/opt/SuiteSparse/GraphBLAS"
 
 // GB_BUILD_PATH: the location where GraphBLAS was built.  This is only used
 // if the GraphBLAS cache path cannot be determined by GrB_init.
-#define GB_BUILD_PATH   "/home/faculty/d/davis/master/GraphBLAS/build"
+#define GB_BUILD_PATH   "/home/davis/master/GraphBLAS/build"
 
 // GB_OMP_INC: include directories for OpenMP, if in use by GraphBLAS:
 #define GB_OMP_INC      ""
 
 // GB_LIBRARIES: libraries to link with
-#define GB_LIBRARIES    " -lm -ldl /usr/lib/llvm-10/lib/libomp.so /usr/lib/x86_64-linux-gnu/libpthread.so"
+#define GB_LIBRARIES    " -lm -ldl /usr/lib/gcc/x86_64-linux-gnu/7/libgomp.so /usr/lib/x86_64-linux-gnu/libpthread.so"
 #endif
 
