28af47e3eba788c0b6d8a619f103fcefac3e06b0
Tim Davis <DrTimothyAldenDavis@gmail.com>
2023-04-04 21:59:16 -0500

    GB_dev.h: unset debug

--- Source/GB_dev.h
+++ Source/GB_dev.h
@@ -19,7 +19,7 @@
 
 // to turn on Debug for all of GraphBLAS, uncomment this line:
 // (GraphBLAS will be exceedingly slow; this is for development only)
-#define GB_DEBUG
+// #define GB_DEBUG
 
 // to turn on a very verbose memory trace
 // (GraphBLAS will be exceedingly slow; this is for development only)
