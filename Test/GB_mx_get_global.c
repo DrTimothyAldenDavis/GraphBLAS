d5f5cdfd5b0515d27722777fc06f68f104c22ed8
Tim Davis <DrTimothyAldenDavis@gmail.com>
2023-04-06 08:05:23 -0500

    test125: failing in GB_mx_get_global, line 71

--- Test/GB_mx_get_global.c
+++ Test/GB_mx_get_global.c
@@ -68,6 +68,7 @@ bool GB_mx_get_global       // true if doing malloc_debug
     }
     // mxMalloc, mxCalloc, mxRealloc, and mxFree are not thread safe
     GB_Global_malloc_is_thread_safe_set (false) ;
+    // FIXME: fails in test125:
     ASSERT (GB_Global_nmalloc_get ( ) == 0) ;
     GB_Global_abort_function_set (GB_mx_abort) ;
     GB_Global_malloc_tracking_set (true) ;
