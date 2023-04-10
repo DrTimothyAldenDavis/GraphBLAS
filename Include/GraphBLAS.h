8152a9e8ca5b2f427e68ee2e35ac384011ab50e6
Tim Davis <davis@tamu.edu>
2023-04-10 14:27:28 -0500

    8.0.0.draft5

--- Include/GraphBLAS.h
+++ Include/GraphBLAS.h
@@ -232,7 +232,7 @@
 
 // The version of this implementation, and the GraphBLAS API version:
 #define GxB_IMPLEMENTATION_NAME "SuiteSparse:GraphBLAS"
-#define GxB_IMPLEMENTATION_DATE "(DRAFT4) Apr 3, 2023"
+#define GxB_IMPLEMENTATION_DATE "(DRAFT5) Apr 10, 2023"
 #define GxB_IMPLEMENTATION_MAJOR 8
 #define GxB_IMPLEMENTATION_MINOR 0
 #define GxB_IMPLEMENTATION_SUB   0
