e78a37367242ff3160d75879092d2f0cdc468963
Tim Davis <DrTimothyAldenDavis@gmail.com>
2023-04-17 21:16:26 -0500

    fixmes flagged

--- cmake_modules/GraphBLAS_version.cmake
+++ cmake_modules/GraphBLAS_version.cmake
@@ -5,8 +5,10 @@
 # SuiteSparse:GraphBLAS, Timothy A. Davis, (c) 2017-2023, All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+#-------------------------------------------------------------------------------
+
 # version of SuiteSparse:GraphBLAS
-set ( GraphBLAS_DATE "(DRAFT7) Apr 17, 2023" )
+set ( GraphBLAS_DATE "(DRAFT7) Apr 17, 2023" )  # FIXME: set the date
 set ( GraphBLAS_VERSION_MAJOR 8 )
 set ( GraphBLAS_VERSION_MINOR 0 )
 set ( GraphBLAS_VERSION_SUB   0 )
