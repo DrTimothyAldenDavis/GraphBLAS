403026e6b9c7961645d904cf05b885890356ca3e
Tim Davis <davis@tamu.edu>
2023-04-18 19:55:57 -0500

    static header control moved out of GB_dev.h

--- Source/Template/GB_dev.h
+++ Source/Template/GB_dev.h
@@ -28,15 +28,5 @@
 // to turn on diagnostic timings.  See GrB.timing in the @GrB interface.
 // #define GB_TIMING
 
-// By default, many internal temporary matrices use statically allocated
-// headers to reduce the number of calls to malloc/free.  This works fine for
-// matrices on the CPU, but the static headers do not get automatically
-// transfered to the GPU.  Only dynamically allocated headers, allocated by
-// rmm_wrap_malloc, get transfered.  Set this to 1 to turn off static headers
-// (required for CUDA; see GB_static_headers.h).  Leave static headers
-// enabled by default by leaving this commented out or setting GBNSTATIC to 0.
-// #undef  GBNSTATIC
-// #define GBNSTATIC 1
-
 #endif
 
