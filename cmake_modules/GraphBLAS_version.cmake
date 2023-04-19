9e7295aece5ff6298e69ec63856c51cbbd37df07
Tim Davis <davis@tamu.edu>
2023-04-19 08:49:35 -0500

    nearing draft8

--- cmake_modules/GraphBLAS_version.cmake
+++ cmake_modules/GraphBLAS_version.cmake
@@ -8,7 +8,7 @@
 #-------------------------------------------------------------------------------
 
 # version of SuiteSparse:GraphBLAS
-set ( GraphBLAS_DATE "(DRAFT7) Apr 17, 2023" )  # FIXME: set the date
+set ( GraphBLAS_DATE "(DRAFT8) Apr 19, 2023" )  # FIXME: set the date
 set ( GraphBLAS_VERSION_MAJOR 8 )
 set ( GraphBLAS_VERSION_MINOR 0 )
 set ( GraphBLAS_VERSION_SUB   0 )
