7b7276e4f762e10bb5e0e7f1ebd474ae9cdc0954
Tim Davis <DrTimothyAldenDavis@gmail.com>
2023-05-18 14:12:22 -0500

    v8.0.0

--- cmake_modules/GraphBLAS_version.cmake
+++ cmake_modules/GraphBLAS_version.cmake
@@ -8,7 +8,7 @@
 #-------------------------------------------------------------------------------
 
 # version of SuiteSparse:GraphBLAS
-set ( GraphBLAS_DATE "(beta1) May 18, 2023" )  # FIXME: remove beta1 for v8.0.0
+set ( GraphBLAS_DATE "May 18, 2023" )
 set ( GraphBLAS_VERSION_MAJOR 8 )
 set ( GraphBLAS_VERSION_MINOR 0 )
 set ( GraphBLAS_VERSION_SUB   0 )
