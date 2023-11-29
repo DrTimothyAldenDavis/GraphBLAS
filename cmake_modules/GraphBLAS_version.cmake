4ec49002d5e49a01f5ebb6c739c38caea590aa1e
Tim Davis <davis@tamu.edu>
2023-11-28 19:57:13 -0600

    sync with 8.3.0

--- cmake_modules/GraphBLAS_version.cmake
+++ cmake_modules/GraphBLAS_version.cmake
@@ -8,15 +8,15 @@
 #-------------------------------------------------------------------------------
 
 # version of SuiteSparse:GraphBLAS
-set ( GraphBLAS_DATE "Nov 26, 2023" )
-set ( GraphBLAS_VERSION_MAJOR 9 )
-set ( GraphBLAS_VERSION_MINOR 0 )
-set ( GraphBLAS_VERSION_SUB   1 )
+set ( GraphBLAS_DATE "Dec 30, 2023" )
+set ( GraphBLAS_VERSION_MAJOR 9 CACHE STRING "" FORCE )
+set ( GraphBLAS_VERSION_MINOR 0 CACHE STRING "" FORCE )
+set ( GraphBLAS_VERSION_SUB   1 CACHE STRING "" FORCE )
 
 # GraphBLAS C API Specification version, at graphblas.org
-set ( GraphBLAS_API_DATE "Dec 31, 2023" )
+set ( GraphBLAS_API_DATE "Nov 15, 2021" )
 set ( GraphBLAS_API_VERSION_MAJOR 2 )
-set ( GraphBLAS_API_VERSION_MINOR 1 )
+set ( GraphBLAS_API_VERSION_MINOR 0 )
 set ( GraphBLAS_API_VERSION_SUB   0 )
 
 message ( STATUS "Building SuiteSparse:GraphBLAS version: v"
