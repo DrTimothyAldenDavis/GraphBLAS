fbbc5fa7336cfa80b892b5a017e7b0737022aad4
Tim Davis <davis@tamu.edu>
2024-01-04 22:38:03 -0600

    9.0.2: cuda on

--- Include/GraphBLAS.h
+++ Include/GraphBLAS.h
@@ -1,4 +1,4 @@
-// SuiteSparse:GraphBLAS 9.0.1
+// SuiteSparse:GraphBLAS 9.0.2
 //------------------------------------------------------------------------------
 // GraphBLAS.h: definitions for the GraphBLAS package
 //------------------------------------------------------------------------------
@@ -133,7 +133,7 @@
 //------------------------------------------------------------------------------
 
 #ifndef GRAPHBLAS_HAS_CUDA
-/* #undef GRAPHBLAS_HAS_CUDA */
+#define GRAPHBLAS_HAS_CUDA
 #endif
 
 //------------------------------------------------------------------------------
@@ -229,7 +229,7 @@
 #define GxB_IMPLEMENTATION_DATE "Jan XX, 2024"
 #define GxB_IMPLEMENTATION_MAJOR 9
 #define GxB_IMPLEMENTATION_MINOR 0
-#define GxB_IMPLEMENTATION_SUB   1
+#define GxB_IMPLEMENTATION_SUB   2
 #define GxB_SPEC_DATE "Dec 22, 2023"
 #define GxB_SPEC_MAJOR 2
 #define GxB_SPEC_MINOR 1
