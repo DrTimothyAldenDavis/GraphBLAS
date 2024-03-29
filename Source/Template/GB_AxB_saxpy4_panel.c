271a0d8c868a91ade3ee509ba2ca2f5c3d11cc3f
Tim Davis <davis@tamu.edu>
2024-03-29 09:46:35 -0500

    .

--- Source/Template/GB_AxB_saxpy4_panel.c
+++ Source/Template/GB_AxB_saxpy4_panel.c
@@ -121,110 +121,28 @@
 
         case 1 : 
 
-            if (A_iso)
+            for (int64_t kA = 0 ; kA < anvec ; kA++)
             {
-                // A is iso
-                GB_DECLAREA (aik) ;
-                GB_GETA (aik, Ax, 0, true) ;
-                if (A_is_hyper)
-                {
-                    // A is hyper and iso
-                    for (int64_t kA = 0 ; kA < anvec ; kA++)
-                    {
-                        // get A(:,k)
-                        const int64_t k = Ah [kA] ;
-                        // get B(k,j1)
-                        #if GB_B_IS_BITMAP
-                        const int8_t gb0 = Gb [k] ;
-                        if (!gb0) continue ;
-                        #endif
-                        // H += A(:,k)*B(k,j1)
-                        GB_DECLAREB (gk0) ;
-                        GB_GETB (gk0, Gx, k, B_iso) ;
-                        const int64_t pA_end = Ap [kA+1] ;
-                        for (int64_t pA = Ap [kA] ; pA < pA_end ; pA++)
-                        { 
-                            // H (i) += aik * gk0
-                            GB_HX_COMPUTE (Ai [pA], Ai [pA], gk0, 1, 0) ;
-                        }
-                    }
-                }
-                else
-                {
-                    // A is sparse and iso
-                    for (int64_t k = 0 ; k < anvec ; k++)
-                    {
-                        // get B(k,j1)
-                        #if GB_B_IS_BITMAP
-                        const int8_t gb0 = Gb [k] ;
-                        if (!gb0) continue ;
-                        #endif
-                        // H += A(:,k)*B(k,j1)
-                        GB_DECLAREB (gk0) ;
-                        GB_GETB (gk0, Gx, k, B_iso) ;
-                        const int64_t pA_end = Ap [k+1] ;
-                        for (int64_t pA = Ap [k] ; pA < pA_end ; pA++)
-                        { 
-                            // H (i) += aik * gk0
-                            GB_HX_COMPUTE (Ai [pA], Ai [pA], gk0, 1, 0) ;
-                        }
-                    }
+                // get A(:,k)
+                const int64_t k = GBH_A (Ah, kA) ;
+                // get B(k,j1)
+                #if GB_B_IS_BITMAP
+                const int8_t gb0 = Gb [k] ;
+                if (!gb0) continue ;
+                #endif
+                // H += A(:,k)*B(k,j1)
+                GB_DECLAREB (gk0) ;
+                GB_GETB (gk0, Gx, k, B_iso) ;
+                const int64_t pA_end = Ap [kA+1] ;
+                for (int64_t pA = Ap [kA] ; pA < pA_end ; pA++)
+                { 
+                    // aik = A (i,j)
+                    GB_DECLAREA (aik) ;
+                    GB_GETA (aik, Ax, pA, A_iso) ;
+                    // H (i) += aik * gk0
+                    GB_HX_COMPUTE (Ai [pA], Ai [pA], gk0, 1, 0) ;
                 }
             }
-            else
-            {
-                if (A_is_hyper)
-                {
-                    // A is hyper and non-iso
-                    for (int64_t kA = 0 ; kA < anvec ; kA++)
-                    {
-                        // get A(:,k)
-                        const int64_t k = Ah [kA] ;
-                        // get B(k,j1)
-                        #if GB_B_IS_BITMAP
-                        const int8_t gb0 = Gb [k] ;
-                        if (!gb0) continue ;
-                        #endif
-                        // H += A(:,k)*B(k,j1)
-                        GB_DECLAREB (gk0) ;
-                        GB_GETB (gk0, Gx, k, B_iso) ;
-                        const int64_t pA_end = Ap [kA+1] ;
-                        for (int64_t pA = Ap [kA] ; pA < pA_end ; pA++)
-                        { 
-                            // aik = A (i,j)
-                            GB_DECLAREA (aik) ;
-                            GB_GETA (aik, Ax, pA, false) ;
-                            // H (i) += aik * gk0
-                            GB_HX_COMPUTE (Ai [pA], Ai [pA], gk0, 1, 0) ;
-                        }
-                    }
-                }
-                else
-                {
-                    // A is sparse and non-iso
-                    for (int64_t k = 0 ; k < anvec ; k++)
-                    {
-                        // get B(k,j1)
-                        #if GB_B_IS_BITMAP
-                        const int8_t gb0 = Gb [k] ;
-                        if (!gb0) continue ;
-                        #endif
-                        // H += A(:,k)*B(k,j1)
-                        GB_DECLAREB (gk0) ;
-                        GB_GETB (gk0, Gx, k, B_iso) ;
-                        const int64_t pA_end = Ap [k+1] ;
-                        for (int64_t pA = Ap [k] ; pA < pA_end ; pA++)
-                        { 
-                            // aik = A (i,j)
-                            GB_DECLAREA (aik) ;
-                            GB_GETA (aik, Ax, pA, false) ;
-                            // H (i) += aik * gk0
-                            GB_HX_COMPUTE (Ai [pA], Ai [pA], gk0, 1, 0) ;
-                        }
-                    }
-                }
-            }
-
             break ;
 
         default:;
