8d5c7d274556d9a1f7a06bf1bd9d1fd28d4d3c19
Tim Davis <davis@tamu.edu>
2024-04-09 12:25:38 -0500

    9.1.1.beta2

--- cmake_modules/GraphBLAS_version.cmake
+++ cmake_modules/GraphBLAS_version.cmake
@@ -8,10 +8,10 @@
 #-------------------------------------------------------------------------------
 
 # version of SuiteSparse:GraphBLAS
-set ( GraphBLAS_DATE "Apr XX, 2024" )
+set ( GraphBLAS_DATE "Apr 9, 2024" )
 set ( GraphBLAS_VERSION_MAJOR 9 CACHE STRING "" FORCE )
 set ( GraphBLAS_VERSION_MINOR 1 CACHE STRING "" FORCE )
-set ( GraphBLAS_VERSION_SUB   2 CACHE STRING "" FORCE )
+set ( GraphBLAS_VERSION_SUB   1 CACHE STRING "" FORCE )
 
 # GraphBLAS C API Specification version, at graphblas.org
 set ( GraphBLAS_API_DATE "Dec 22, 2023" )
