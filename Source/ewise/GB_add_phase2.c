758ac752527029f41b7e4b2e02b20cdcbbb7381f
Tim Davis <DrTimothyAldenDavis@gmail.com>
2024-10-05 14:41:35 -0500

    .

--- Source/ewise/GB_add_phase2.c
+++ Source/ewise/GB_add_phase2.c
@@ -538,7 +538,7 @@ GrB_Info GB_add_phase2      // C=A+B, C<M>=A+B, or C<!M>=A+B
                     fadd_idx (z, aij, i, j, bij, i, j, theta) ;
                 if (is_eWiseUnion)
                 { 
-GB_GOTCHA ; // ewiseunion with index binary op, flipped
+GB_GOTCHA ; // generic ewiseunion with index binary op, flipped
                     #define GB_IS_EWISEUNION 1
                     #include "ewise/template/GB_add_template.c"
                 }
