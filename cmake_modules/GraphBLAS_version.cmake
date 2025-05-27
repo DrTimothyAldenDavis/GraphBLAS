eca4291f6b93ed33015e1fce28f9da06e2b3076a
Tim Davis <DrTimothyAldenDavis@gmail.com>
2025-05-26 20:49:24 -0500

    target release date for 10.1.0

--- cmake_modules/GraphBLAS_version.cmake
+++ cmake_modules/GraphBLAS_version.cmake
@@ -8,7 +8,7 @@
 #-------------------------------------------------------------------------------
 
 # version of SuiteSparse:GraphBLAS
-set ( GraphBLAS_DATE "May 20, 2025" )
+set ( GraphBLAS_DATE "June 1, 2025" )
 set ( GraphBLAS_VERSION_MAJOR 10 CACHE STRING "" FORCE )
 set ( GraphBLAS_VERSION_MINOR 1 CACHE STRING "" FORCE )
 set ( GraphBLAS_VERSION_SUB   0 CACHE STRING "" FORCE )
