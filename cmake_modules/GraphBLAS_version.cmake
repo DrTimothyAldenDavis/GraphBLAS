366175eeac33de3c43bbff438ef418c6b7bb5ed0
Tim Davis <DrTimothyAldenDavis@gmail.com>
2025-06-03 11:25:52 -0500

    date

--- cmake_modules/GraphBLAS_version.cmake
+++ cmake_modules/GraphBLAS_version.cmake
@@ -8,7 +8,7 @@
 #-------------------------------------------------------------------------------
 
 # version of SuiteSparse:GraphBLAS
-set ( GraphBLAS_DATE "June 2, 2025" )
+set ( GraphBLAS_DATE "June 3, 2025" )
 set ( GraphBLAS_VERSION_MAJOR 10 CACHE STRING "" FORCE )
 set ( GraphBLAS_VERSION_MINOR 1 CACHE STRING "" FORCE )
 set ( GraphBLAS_VERSION_SUB   1 CACHE STRING "" FORCE )
