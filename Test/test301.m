f0f093512565b0bc564fb4272adadf97e9079720
Tim Davis <DrTimothyAldenDavis@gmail.com>
2025-10-06 11:11:26 -0500

    test301: full test coverage of assign method 27

--- Test/test301.m
+++ Test/test301.m
@@ -19,11 +19,26 @@ for m = [1 5 10 200]
                 for trial = 1:30
                     A = GB_mex_random (m, n, t1, 0, seed) ; seed = seed + 1 ;
                     C = GB_mex_random (m, n, t2, 0, seed) ; seed = seed + 1 ;
+                    if (mod (trial, 4) == 0)
+                        C (:,end) = 1 ;
+                    elseif (mod (trial, 4) == 1)
+                        C (1:2:end,end) = 1 ;
+                    end
                     M = C ;
 
                     % C<C,struct> += A
                     C1 = GB_mex_assign_method27 (C, 'plus', A) ;
                     C2 = GB_mex_assign (C, M, 'plus', A, [ ], [ ], desc, 0) ;
+                    if (~isequal (C1, C2))
+                        C1
+                        C2
+                    end
+                    assert (isequal (C1, C2)) ;
+
+                    % A<A,struct> += C
+                    M = A ;
+                    C1 = GB_mex_assign_method27 (A, 'plus', C) ;
+                    C2 = GB_mex_assign (A, M, 'plus', C, [ ], [ ], desc, 0) ;
                     assert (isequal (C1, C2)) ;
                 end
             end
