ba8140e8c981fb3e3cb908d5d5d7b6915d0c1656
Tim Davis <DrTimothyAldenDavis@gmail.com>
2025-11-05 15:38:15 -0600

    dup: do not wait on input matrix (for v10.3.0)

--- Test/testall.m
+++ Test/testall.m
@@ -123,6 +123,7 @@ set_malloc_debug (mdebug, 1) ;
 logstat ('test300'    ,t, J0   , F0   ) ; % print function for a type
 logstat ('test301'    ,t, J40  , F11  ) ; % assign method27, C<C,struct>+=A
 logstat ('test302'    ,t, J0   , F0   ) ; % GPU controls
+logstat ('test155'    ,t, J40  , F10  , [0 2 4]) ; % setElement, removeElement
 
 % < 1 second: debug_off
 set_malloc_debug (mdebug, 0) ;
@@ -142,7 +143,6 @@ logstat ('test247'    ,t, J40  , F10  ) ; % GrB_mxm: fine Hash method
 logstat ('test109'    ,t, J4040, F1100) ; % terminal monoid with user-defn type
 logstat ('test138'    ,s, J40  , F10  ) ; % assign, coarse-only in IxJ slice
 logstat ('test172'    ,t, J40  , F10  ) ; % eWiseMult with M bitmap/full
-logstat ('test155'    ,t, J40  , F10  , [0 2 4]) ; % setElement, removeElement
 logstat ('test174'    ,t, J40  , F10  ) ; % GrB_assign C<A>=A
 logstat ('test203'    ,t, J4   , F1   ) ; % iso subref
 logstat ('test213'    ,t, J40  , F10  ) ; % iso assign (method 05d)
--- cmake_modules/GraphBLAS_version.cmake
+++ cmake_modules/GraphBLAS_version.cmake
@@ -8,9 +8,9 @@
 #-------------------------------------------------------------------------------
 
 # version of SuiteSparse:GraphBLAS
-set ( GraphBLAS_DATE "Nov 1, 2025" )
+set ( GraphBLAS_DATE "Nov 10, 2025" )
 set ( GraphBLAS_VERSION_MAJOR 10 CACHE STRING "" FORCE )
-set ( GraphBLAS_VERSION_MINOR 2 CACHE STRING "" FORCE )
+set ( GraphBLAS_VERSION_MINOR 3 CACHE STRING "" FORCE )
 set ( GraphBLAS_VERSION_SUB   0 CACHE STRING "" FORCE )
 
 # GraphBLAS C API Specification version, at graphblas.org
