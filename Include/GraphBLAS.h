bf8eb7cbab0b9bc5ba31b4bee97572aa4ffd9155
Tim Davis <DrTimothyAldenDavis@gmail.com>
2025-11-13 18:29:27 -0500

    merge libgraphblas.so and libgraphblas_cuda.so into a single libgraphblas.so

--- Include/GraphBLAS.h
+++ Include/GraphBLAS.h
@@ -1,4 +1,4 @@
-// SuiteSparse:GraphBLAS 10.3.0
+// SuiteSparse:GraphBLAS 10.3.1
 //------------------------------------------------------------------------------
 // GraphBLAS.h: definitions for the GraphBLAS package
 //------------------------------------------------------------------------------
@@ -162,7 +162,7 @@
 //------------------------------------------------------------------------------
 
 #ifndef GRAPHBLAS_HAS_CUDA
-/* #undef GRAPHBLAS_HAS_CUDA */
+#define GRAPHBLAS_HAS_CUDA
 #endif
 
 //------------------------------------------------------------------------------
@@ -289,7 +289,7 @@
 #define GxB_IMPLEMENTATION_DATE "Nov 10, 2025"
 #define GxB_IMPLEMENTATION_MAJOR 10
 #define GxB_IMPLEMENTATION_MINOR 3
-#define GxB_IMPLEMENTATION_SUB   0
+#define GxB_IMPLEMENTATION_SUB   1
 #define GxB_SPEC_DATE "Dec 22, 2023"
 #define GxB_SPEC_MAJOR 2
 #define GxB_SPEC_MINOR 1
--- cmake_modules/GraphBLAS_version.cmake
+++ cmake_modules/GraphBLAS_version.cmake
@@ -11,7 +11,7 @@
 set ( GraphBLAS_DATE "Nov 10, 2025" )
 set ( GraphBLAS_VERSION_MAJOR 10 CACHE STRING "" FORCE )
 set ( GraphBLAS_VERSION_MINOR 3 CACHE STRING "" FORCE )
-set ( GraphBLAS_VERSION_SUB   0 CACHE STRING "" FORCE )
+set ( GraphBLAS_VERSION_SUB   1 CACHE STRING "" FORCE )
 
 # GraphBLAS C API Specification version, at graphblas.org
 set ( GraphBLAS_API_DATE "Dec 22, 2023" )
