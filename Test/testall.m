06876079385d17010cf732e68fe338d9e6782330
Tim Davis <drtimothyaldendavis@gmail.com>
2026-08-02 18:02:41 -0500

    additional testing

--- Test/testall.m
+++ Test/testall.m
@@ -122,6 +122,7 @@ set_malloc_debug (mdebug, 1) ;
 
 % < 1 second: debug_on
 logstat ('test01'     ,t, J40  , F10  ) ; % error handling
+logstat ('test307'    ,t, J4   , F1   ) ; % arenas
 logstat ('test306'    ,t, J0   , F0   ) ; % arenas
 logstat ('test305'    ,t, J0   , F0   ) ; % arenas
 logstat ('test304'    ,t, J0   , F0   ) ; % extractElement
